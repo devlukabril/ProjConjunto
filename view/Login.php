be12db190664b32ba2a7b0c9bba0b5e88ba09b68
devlucas <lukabril@hotmail.com>
2023-06-03 20:06:58 -0300

    inicio do proj

--- view/Login.php
+++ view/Login.php
@@ -5,10 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-
-    <h1>Página de Login foda!</h1>
-    
+<div class="container">
+    <h2>Login</h2>
+    <form>
+      <div class="form-group">
+        <label for="username">Usuário:</label>
+        <input type="text" id="username" name="username" required>
+      </div>
+      <div class="form-group">
+        <label for="password">Senha:</label>
+        <input type="password" id="password" name="password" required>
+      </div>
+      <div class="form-group">
+        <input type="submit" value="Entrar">
+      </div>
+    </form>
 </body>
 </html>
